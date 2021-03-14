83a4671edf47890551053945fc51607458619dd5
Andrew Goncharov <71984230+Gazuz33@users.noreply.github.com>
2021-03-14 23:40:34 +0300

    fix: mb correct

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 module.exports = function reverse (n) {
-  var num="";
-    var m = toString(n);
-  for (var i=m.length-1;i>=0;i--)
-    {
-      num += m[i];
-    }
-  return num;
+return Number(Math.abs(n).toString().split("").reverse().join(""));
 }
